2e5101cc4a8bc5218e0a7cbe183b6cb8c544fcc4
Попович Екатерина <71285738+qnopka@users.noreply.github.com>
2021-03-22 09:53:52 +0200

    upd

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,12 +1,36 @@
+$('.responsive').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    centerMode: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    mobileFirst: true,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
 
-$(document).on('ready', function() {
-
-    $(".center").slick({
-        dots: true,
-        infinite: true,
-        centerMode: true,
-        slidesToShow: 1,
-        slidesToScroll: 3
-    });
-
+    ]
 });
+
